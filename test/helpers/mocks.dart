56c8cc8bb4b9ca2e4574c1afc3b6b6b1878f116e
Boris Gautier <tchoukouahaboris@outlook.fr>
2026-05-13 10:17:43 +0100

    test: add widget tests for ExampleListPage and ExampleDetailPage + MockExampleBloc

--- test/helpers/mocks.dart
+++ test/helpers/mocks.dart
@@ -1,8 +1,10 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:flutter_template_app/core/network/network_info.dart';
 import 'package:flutter_template_app/features/example/data/datasources/example_remote_datasource.dart';
 import 'package:flutter_template_app/features/example/domain/repositories/example_repository.dart';
 import 'package:flutter_template_app/features/example/domain/usecases/get_examples_usecase.dart';
+import 'package:flutter_template_app/features/example/presentation/bloc/example_bloc.dart';
 
 // Mocks centralisés — importer dans les tests avec :
 // import '../../helpers/mocks.dart';
@@ -10,3 +12,5 @@ class MockExampleRepository extends Mock implements ExampleRepository {}
 class MockExampleRemoteDataSource extends Mock implements ExampleRemoteDataSource {}
 class MockNetworkInfo extends Mock implements NetworkInfo {}
 class MockGetExamplesUseCase extends Mock implements GetExamplesUseCase {}
+class MockExampleBloc extends MockBloc<ExampleEvent, ExampleState>
+    implements ExampleBloc {}
